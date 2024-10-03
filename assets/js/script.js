1dc2fd4dac2318dcc6648bd8602213032ffe8143
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 05:34:24 +0000

    run initGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-
-
+const playBoard = document.querySelector(".game-space");
+let foodX = 13, foodY = 10;
 const initGame = () => {
-    let htmlMarkup = ` <div class="food" style="grid-area: "></div>`;
+    let htmlMarkup = ` <div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
 }
+initGame();
