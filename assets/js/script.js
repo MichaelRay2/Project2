4ac4221022b8beda2d94aa2098ce0b053deb95d2
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 06:10:25 +0000

    Assigning speed based on arrow clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 const playBoard = document.querySelector(".game-space");
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
-let speedX = 0, speedY = -1;
+let speedX = 0, speedY = 0;
 const moveFood = () => {
     // Assigning random x and y co-ordinate for food
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
-const changeDirection = () => {
-    console.log(e);
+const changeDirection = (e) => {
+    if(e.key === "ArrowUp"){
+        speedX = 0;
+        speedY = -1
+    }else if(e.key === "ArrowDown"){
+        speedX = 0;
+        speedY = 1;
+    }else if(e.key === "ArrowLeft"){
+        speedX = -1;
+        speedY = 0;
+    }else if(e.key === "ArrowRight"){
+        speedX = 1;
+        speedY = 0;
+    }
 }
+    
+
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
