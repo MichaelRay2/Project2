84942d02bbd9fc8504440149f90a0935b0b2da9f
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 06:02:34 +0000

    Added speed variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 const playBoard = document.querySelector(".game-space");
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
+let speedX = 0, speedY = -1;
 const moveFood = () => {
     // Assigning random x and y co-ordinate for food
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
-
+const changeDirection = () => {
+    console.log(e);
+}
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
