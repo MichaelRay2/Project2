bb0ed5a3d14829a524d265ac65fd9a976d68a283
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 05:39:46 +0000

    Defined const to assign random food placement value to

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const playBoard = document.querySelector(".game-space");
 let foodX = 13, foodY = 10;
+const moveFood = () => {
+    foodX = math.floor(Math.random() * 30) + 1
+}
 const initGame = () => {
     let htmlMarkup = ` <div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
