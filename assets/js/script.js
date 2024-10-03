6d8464dd6ebfd24de8a51b40fb15ba50207e3e12
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 05:42:51 +0000

    Comment added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".game-space");
 let foodX = 13, foodY = 10;
 const moveFood = () => {
+    // Assigning random x and y co-ordinate for food
     foodX = math.floor(Math.random() * 30) + 1;
     foodY = math.floor(math.random() * 30) + 1;
 }
