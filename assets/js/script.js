cf1ec1d8fa00dd6ec5d3c1576727c293fc270b47
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 05:55:18 +0000

    Capital letter fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 const playBoard = document.querySelector(".game-space");
 let foodX, foodY;
-let snakeX, snakeY;
+let snakeX = 5, snakeY = 10;
 const moveFood = () => {
     // Assigning random x and y co-ordinate for food
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 const initGame = () => {
-    let htmlMarkup = ` <div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-    htmlMarkup += ` <div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    htmlMarkup += `<div class="food" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
 moveFood();
