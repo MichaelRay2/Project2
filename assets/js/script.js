7cc501b70265d557d70128d9b80d43b8759c91a1
Michael Ray <michaelray2809@yahoo.com>
2024-10-03 05:47:12 +0000

    Fixed bug by adding cap letters

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@ const playBoard = document.querySelector(".game-space");
 let foodX, foodY;
 const moveFood = () => {
     // Assigning random x and y co-ordinate for food
-    foodX = math.floor(Math.random() * 30) + 1;
-    foodY = math.floor(math.random() * 30) + 1;
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
 }
 const initGame = () => {
     let htmlMarkup = ` <div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
+moveFood();
 initGame();
