6e52b069be853f7c4f435501214614f546a715a6
Michael Ray <michaelray2809@yahoo.com>
2024-11-04 08:12:48 +0000

    Used for loop to add cell to snake's body

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".game-space");
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
+let snakeBody = [];
 let speedX = 0, speedY = 0;
 const moveFood = () => {
     // Assigning random x and y co-ordinate for food
@@ -28,14 +29,21 @@ const changeDirection = (e) => {
 
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-
+    // Check to see if snake ate the food
     if(snakeX === foodX && snakeY == foodY){
         moveFood();
+        snakeBody.push([foodX, foodY]); // push food position to array of snake body
+        console.log(snakeBody);
     }
 
     // Updating snake's head position based on the current speed
     snakeX += speedX;
     snakeY += speedY;
+
+    for(let i = 0; i < snakeBody.length; i++){
+        // Adding a div element for each cell of the snake's body
+        htmlMarkup += `<div class="head" style="grid-adrea": ${snakeBody[i][1]} /> ${snakeBody[i][0]}"></div>`;
+    }
 
 
     htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
